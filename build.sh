fb3ed96e2806248bfa59536cb9f7e82ce741f4e7
Ian Chen <ian@Ians-MBP.largitdata.office>
2018-07-03 16:41:13 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -ex
+rm -rf ./_site/*
 JEKYLL_ENV=production bundle exec jekyll build
 cd _site
 echo "ianchen.tw" > CNAME
 rm -rf build.sh
 git add .
-git commit -m $(date +%s)
-git push origin master
+git commit --amend --no-edit
+git push origin master -f
 cd ..
 curl -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ID}/purge_cache" \
      -H "X-Auth-Email: ${CF_EMAIL}" \
